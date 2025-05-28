60aa6f1c6e05c7657d4357c9e35c0b213606dac7
William L. Thomson Jr. <w@wltjr.com>
2025-05-28 14:16:37 -0400

    src/main.cpp: Modified q-learning params, learning & explore vs exploit
    - Decrease power on learning, ensure q-values always < 1
    - Decrease epsilon decay rate and minimum, lengthns exploration to ~2k
      episodes from ~15.

--- src/main.cpp
+++ src/main.cpp
@@ -193,11 +193,11 @@ void train(args &args,
     cv::cvtColor(cv::imread("templates/cannon.png"), cannon, cv::COLOR_RGB2GRAY);
 
     // q-learning parameters
-    alpha = 0.01;               // learning rate
+    alpha = 0.001;              // learning rate
     gamma = 0.0095;             // discount factor
     epsilon = 1.0;              // exploration rate (starting value)
-    epsilon_min = 0.1;          // minimum exploration rate
-    epsilon_decay = 0.9999;      // decay rate for exploration
+    epsilon_min = 0.01;         // minimum exploration rate
+    epsilon_decay = 0.999999;   // decay rate for exploration
 
     max_episode = -1;
     max_score = -1;
